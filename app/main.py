d5dbbdf7ec7bc14c31d0184660564b848f996422
Aydan <aydanhawken@icloud.com>
2023-09-03 22:37:20 +1200

    Search system (kinda works)

--- app/main.py
+++ app/main.py
@@ -7,16 +7,34 @@ import ui.startup
 import ui.ole_login
 import ui.scanner_setup
 import time
+import os
+import urllib.request
 from utils.dispatch_group import DispatchGroup
 
 
 root = Tk()
 root.withdraw()
 
+def download_image(user_id):
+    # Create the folder != exists
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
+    image_filename = f'user_{user_id}.jpg'
+    image_path = os.path.join('images', image_filename)
+
+    if not os.path.exists(image_path):
+        img_url = f"https://ole.saintkentigern.com/portrait.php?id={user_id}&size=constrain200"
+        urllib.request.urlretrieve(img_url, image_path)
+        print(f"Image downloaded and saved as {image_path}")
+    else:
+        print("Image already exists")
+
+    return
 
 def startup():
     startup_window = ui.startup.Window(root)
-
+    
     # check for OLE login credentials
     oleUsername, olePassword = map(
         lambda x: urllib.parse.unquote(x),
@@ -69,18 +87,22 @@ def startup():
         prompt_login()
 
     def callback():
-        print("Got OLE Credentials: ", oleUsername, olePassword)
-
+        #print("Got OLE Credentials: ", oleUsername, olePassword)
+        user_id = input("Enter user id: ")
+        download_image(user_id)
         def callback():
             # TODO later: Fetch from firebase
 
             # Sign into the OLE
 
+            
+
+            return
+
 
         ui.scanner_setup.Window(root, lambda: callback)
 
     group.notify(callback)
-
 
 if __name__ == "__main__":
     root.after(10, startup)
